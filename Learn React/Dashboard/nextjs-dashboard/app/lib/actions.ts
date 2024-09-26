ab5de34e6a2265c7078cfccefb308a912a391ea2
Kmeadows3 <kmeadows3@gmail.com>
2024-09-26 11:47:09 -0400

    Completed chapter 13, handling errors

--- Learn React/Dashboard/nextjs-dashboard/app/lib/actions.ts
+++ Learn React/Dashboard/nextjs-dashboard/app/lib/actions.ts
@@ -18,6 +18,7 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
   
 
 export async function createInvoice(formData: FormData) {
+
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -26,13 +27,21 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+  try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (e) {
+    return {
+      message: 'Database Error: Failed to create invoice'
+    }
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+ 
+    
 }
 
 
@@ -50,11 +59,17 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try {
+      await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+} catch (e) {
+  return {
+    message: 'Database Error: Failed to edit invoice'
+  }
+}
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -62,6 +77,14 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+  try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+  } catch (e) {
+    return {
+      message: 'Database Error: Failed to delete invoice'
+    }
+  }
   revalidatePath('/dashboard/invoices');
 }
